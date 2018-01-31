edb5c4c6e7b4fd2cc7d603456a6aac2497087e05
Disha <dishagf@gmail.com>
2018-02-01 00:22:49 +0530

    Update models.py

--- djmodel/models.py
+++ djmodel/models.py
@@ -12,11 +12,6 @@ from pygments.styles import get_all_styles
 from vote.models import VoteModel
 
 
-# Create your models here.
-from pygments.lexers import get_all_lexers
-from pygments.styles import get_all_styles
-
-
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
@@ -108,13 +103,6 @@ class File(VoteModel, models.Model):
 
     class Meta:
         ordering = ['size', 'time_added']
-
-    no_of_stars = models.IntegerField()
-    file_data = models.BinaryField()
-
-    class Meta:
-        ordering = ['size']
-
 
 
 class File_Permission(models.Model):
